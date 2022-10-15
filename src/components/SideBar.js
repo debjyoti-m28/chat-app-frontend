2943f2396b2ea3b6c7b5dcc7b4a7a83028bc4668
Debjyoti mondal <91774344+debjyoti-m28@users.noreply.github.com>
2022-10-15 19:52:06 +0530

    Added side bar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,74 @@
-import React from 'react'
-import { Box, Tooltip, Button, Text, Menu ,MenuButton } from '@chakra-ui/react';
-import { BellIcon } from '@chakra-ui/icons';
+import {React, useState} from 'react'
+import { useToast,Input, Box, Tooltip, Button, Text, Menu ,MenuButton, Avatar, MenuList, MenuItem, MenuDivider } from '@chakra-ui/react';
+import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons';
+import { ChatState } from '../context/ChatProvider';
+import Profile from './Profile';
+import { useHistory } from "react-router-dom"
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+} from '@chakra-ui/react'
+import axios from "axios"
 
 const SideBar = () => {
-//   const [search, setSearch] = useState("");
-//   const [searchResult, setSearchResult] = useState([]);
-//   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
 //   const [loadingChat, setLoadingChat] = useState();
+
+const { user } = ChatState();
+const history = useHistory();
+const { isOpen, onOpen, onClose } = useDisclosure()
+const toast = useToast();
+// const btnRef = React.useRef()
+
+const logoutHandler = () =>{
+  localStorage.removeItem("userInfo");
+  history.push("/");
+}
+
+const handleSearch = async() =>{
+  if(!search){
+    toast({
+          title: 'Please Enter something in search!',
+          status: 'warning',
+          duration: 5000,
+          isClosable: true,
+          position: "top-left",
+        });
+        return;
+  }
+
+  try{
+    setLoading(true);
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/user?search=${search}`, config)
+    setLoading(false);
+    setSearchResult(data);
+  }catch(err){
+        toast({
+          title: 'Error Occured!',
+          description: "Failed to Load thr Search Results...",
+          status: 'error',
+          duration: 5000,
+          isClosable: true,
+          position: "bottom-left",
+        });
+        return;
+  }
+}
 
   return (
     <>
@@ -23,7 +85,7 @@ const SideBar = () => {
        hasArrow 
        placement='bottom-end'
        >
-        <Button varient="ghost">
+        <Button varient="ghost" onClick={onOpen}>
             <i class="fas fa-search"></i>
             <Text
             display={{base: "none", md: "flex"}} 
@@ -40,11 +102,59 @@ const SideBar = () => {
        <div>
         <Menu>
           <MenuButton p={1}>
-            <BellIcon/>
+            <BellIcon fontSize="2xl" m={1} />
           </MenuButton>
+          {/* <MenuList></MenuList> */}
+        </Menu>
+
+        <Menu>
+            <MenuButton 
+            as={Button} 
+            rightIcon={<ChevronDownIcon/>}
+            >
+             <Avatar 
+             size='sm' 
+             cursor='pointer' 
+             name={user.name}
+             src={user.pic}
+             />
+            </MenuButton>
+
+            <MenuList>
+              <Profile user={user}>
+                <MenuItem>My Profile</MenuItem>
+              </Profile>
+              
+              <MenuDivider/>
+
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </MenuList>
         </Menu>
        </div>
      </Box>
+
+     {/* side bar */}
+
+      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay/>
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+          <DrawerBody>
+          <Box display="flex" pb={2}>
+            <Input
+            placeholder="Search by name or email"
+            mr={2}
+            value={search}
+            onChange={(e)=> setSearch(e.target.value)}
+            />
+            <Button 
+            onClick={handleSearch}
+            >Go</Button>
+          </Box>
+        </DrawerBody>
+        </DrawerContent>
+        
+      </Drawer>
     </>
   )
 }
